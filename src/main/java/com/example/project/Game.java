c5d834f7a36fe376d37e252d24325fdaaa986801
D2Z11 <133183204+D2Z11@users.noreply.github.com>
2025-02-28 13:12:38 +0000

    Added code

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -1,16 +1,45 @@
 package com.example.project;
+
 import java.util.ArrayList;
 
+public class Game {
+    public static String determineWinner(Player p1, Player p2, String p1Hand, String p2Hand,
+            ArrayList<Card> communityCards) {
+        // This method should be developed to compare hand rankings in the correct
+        // order, from the highest hand (e.g., Royal Flush) to the lowest (e.g., High
+        // Card).
+        // Tiebreakers (e.g., comparing the highest card in case of two players having
+        // the same hand rank) should be accounted for.
 
-public class Game{
-    public static String determineWinner(Player p1, Player p2,String p1Hand, String p2Hand,ArrayList<Card> communityCards){
+        // Check each scenario
+        switch (p1Hand) {
+            case "Royal Flush":
+                break;
+            case "Straight Flush":
+                break;
+            case "Four of a Kind":
+                break;
+            case "Full House":
+                break;
+            case "Flush":
+                break;
+            case "Straight":
+                break;
+            case "Three of a Kind":
+                break;
+            case "Two Pair":
+                break;
+            case "Pair":
+                break;
+            case "High Card":
+                break;
+        }
+
         return "Error";
     }
 
-    public static void play(){ //simulate card playing
-    
+    public static void play() { // simulate card playing
+
     }
-        
-        
 
 }
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -1,45 +1,79 @@
 package com.example.project;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
-
-public class Player{
+public class Player {
     private ArrayList<Card> hand;
-    private ArrayList<Card> allCards; //the current community cards + hand
-    String[] suits  = Utility.getSuits();
+    private ArrayList<Card> allCards; // the current community cards + hand
+    String[] suits = Utility.getSuits();
     String[] ranks = Utility.getRanks();
-    
-    public Player(){
+
+    public Player() {
         hand = new ArrayList<>();
     }
 
-    public ArrayList<Card> getHand(){return hand;}
-    public ArrayList<Card> getAllCards(){return allCards;}
-
-    public void addCard(Card c){
-        
+    public ArrayList<Card> getHand() {
+        return hand;
     }
 
-    public String playHand(ArrayList<Card> communityCards){      
+    public ArrayList<Card> getAllCards() {
+        return allCards;
+    }
+
+    public void addCard(Card c) {
+        allCards.add(c);
+    }
+
+    public String playHand(ArrayList<Card> communityCards) {
+        
         return "Nothing";
     }
 
-    public void SortCards(){} 
-
-    public ArrayList<Integer> findRankingFrequency(){
-        return new ArrayList<>(); 
+    public void SortCards() {
     }
 
-    public ArrayList<Integer> findSuitFrequency(){
-        return new ArrayList<>(); 
+    public ArrayList<Integer> findRankingFrequency() {
+        ArrayList<Integer> frequencies = new ArrayList<>();
+        // Based on the ranking value switch statement in Utility.java: ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        // Loop through the hand
+        for (int i = 0; i < hand.size(); i++) {
+            // Get the ranking of the current card as a String and then get the value as a number/index
+            int index = Utility.getRankValue(hand.get(i).getRank());
+            // Ensure that the index in frequencie is not null to prevent errors
+            if (frequencies.get(index) != null) {
+                // Increment frequency
+                frequencies.set(index, frequencies.get(index) + 1);
+            } else {
+                // Set to zero if it was previously null
+                frequencies.set(index, 0);
+            }
+        }
+        return frequencies;
     }
 
-   
+    public ArrayList<Integer> findSuitFrequency() {
+        ArrayList<Integer> frequencies = new ArrayList<>();
+        // Based on the ranking value switch statement in Utility.java: ["♠","♥","♣", "♦”]
+        // Loop through the hand
+        for (int i = 0; i < hand.size(); i++) {
+            // Get the suit of the current card as a String and then get the value as a number/index
+            int index = Utility.getSuitValue(hand.get(i).getSuit());
+            // Ensure that the index in frequencie is not null to prevent errors
+            if (frequencies.get(index) != null) {
+                // Increment frequency
+                frequencies.set(index, frequencies.get(index) + 1);
+            } else {
+                // Set to zero if it was previously null
+                frequencies.set(index, 0);
+            }
+        }
+        return frequencies;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return hand.toString();
     }
-
-
-
 
 }
