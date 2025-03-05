90cde8a8f523716c896805c30e142b61a2b1ca2a
D2Z11 <133183204+D2Z11@users.noreply.github.com>
2025-03-05 23:19:13 +0000

    Working code

--- src/main/java/com/example/project/Game.java
+++ src/main/java/com/example/project/Game.java
@@ -10,7 +10,8 @@ public class Game {
         // Card).
         // Tiebreakers (e.g., comparing the highest card in case of two players having
         // the same hand rank) should be accounted for.
-                
+
+        // Get the hand ranking of player one and two to see who's won
         int one = Utility.getHandRanking(p1Hand);
         int two = Utility.getHandRanking(p2Hand);
         if (one > two) {
@@ -18,11 +19,13 @@ public class Game {
         } else if (one < two) {
             return "Player 2 wins!";
         } else {
+            // If the highest card value is greater in p1 than p2 then p1 is the winner in the case of a tie
             if (p1.getHighestCardValue(p1.getHand()) > p2.getHighestCardValue(p2.getHand())) {
                 return "Player 1 wins!";
             } else if (p1.getHighestCardValue(p1.getHand()) < p2.getHighestCardValue(p2.getHand())) {
                 return "Player 2 wins!";
             }
+            // Since high cards are equal it's a tie
             return "Tie!";
         }
     }
--- src/main/java/com/example/project/Player.java
+++ src/main/java/com/example/project/Player.java
@@ -23,18 +23,23 @@ public class Player {
     }
 
     public void addCard(Card c) {
+        // Add the card to both your hand and all the cards
         hand.add(c);
         allCards.add(c);
     }
 
+    // In simplified poker, two players are dealt two private cards each, and there
+    // are three community cards that all players can use.
+    // The goal is to create the best possible 5-card hand using any combination of
+    // the **two cards in hand and the three community cards.**
     public String playHand(ArrayList<Card> communityCards) {
+        // In case the cards haven't already been added to the community cards, add
+        // them!
         if (allCards.size() < 5) {
             allCards.addAll(communityCards);
         }
-        // In simplified poker, two players are dealt two private cards each, and there
-        // are three community cards that all players can use.
-        // The goal is to create the best possible 5-card hand using any combination of
-        // the **two cards in hand and the three community cards.**
+
+        // Ranking frequencies
         int[] rFreq = findRankingFrequency();
         // int[] sFreq = findSuitFrequency();
 
@@ -48,6 +53,7 @@ public class Player {
             if (i == 12) {
                 String first = allCards.get(0).getSuit();
                 for (int z = 1; z < allCards.size(); z++) {
+                    // Just exit if suit is not consistent
                     if (!allCards.get(z).getSuit().equals(first)) {
                         break;
                     }
@@ -57,14 +63,18 @@ public class Player {
                 }
             }
         }
+        // Use h as a count variable to check for consecutive amounts
         int h = 1;
         // Loop through array and find out if there is a 5 consecutive
         for (int i = 0; i < rFreq.length; i++) {
             if (rFreq[i] >= 1) {
                 h++;
+                // If 5 consecutive ranks
                 if (h >= 5) {
+                    // Then compare suits using similar strategy to before
                     String first = allCards.get(0).getSuit();
                     for (int z = 1; z < allCards.size(); z++) {
+                        // Break if inconsistent suit
                         if (!allCards.get(z).getSuit().equals(first)) {
                             break;
                         }
@@ -79,56 +89,65 @@ public class Player {
         }
         // Find 4 cards of same rank
         for (int i = 0; i < rFreq.length; i++) {
+            // Just look for frequency of 4
             if (rFreq[i] == 4) {
                 return "Four of a Kind";
             }
         }
+        // Three is to check three cards of one ranks and two is for two cards of
+        // another rank
         boolean three = false;
         boolean two = false;
         // Find three cards of one rank and two cards of another rank.
         for (int i = 0; i < rFreq.length; i++) {
-            // System.out.println(rFreq[i]);
+            // Check for frequency of 3
             if (rFreq[i] == 3) {
                 if (three) {
                     two = true;
                 }
                 three = true;
             }
+            // Check for frequency of 2
             if (rFreq[i] == 2) {
                 two = true;
             }
         }
+        // Satisfies full house
         if (three && two) {
             return "Full House";
         }
         String first = allCards.get(0).getSuit();
         for (int z = 1; z < allCards.size(); z++) {
+            // If suit inconsistent break
             if (!allCards.get(z).getSuit().equals(first)) {
                 break;
             }
+            // If last item check condition (5 same suits)
             if (z == allCards.size() - 1) {
                 return "Flush";
             }
         }
+        // Use h again like before
         h = 1;
-        // Loop through array and find out if there is a 5 consecutive
+        // Loop through array and find out if there is a 5 consecutive ranks
         for (int i = 0; i < rFreq.length; i++) {
-            System.out.println(rFreq[i]);
+            // Use same strategy as mentioned earlier
             if (rFreq[i] >= 1) {
                 h++;
                 if (h >= 5) {
-                    System.out.println("wtf");
                     return "Straight";
                 }
             } else {
                 h = 0;
             }
         }
+        // Find frequency of 3
         for (int i = 0; i < rFreq.length; i++) {
             if (rFreq[i] == 3) {
                 return "Three of a Kind";
             }
         }
+        // Count for a frequency of 2 cards twice
         int count = 0;
         for (int i = 0; i < rFreq.length; i++) {
             if (rFreq[i] == 2) {
@@ -138,12 +157,15 @@ public class Player {
                 return "Two Pair";
             }
         }
+        // Count for a frequency of two cards once
         for (int i = 0; i < rFreq.length; i++) {
             // System.out.println(rFreq[i]);
             if (rFreq[i] == 2) {
                 return "A Pair";
             }
         }
+        // Get highest card value for hand and community and determine if high card or
+        // nothing
         int max = getHighestCardValue(hand);
         int max2 = getHighestCardValue(communityCards);
         if (max > max2) {
@@ -155,6 +177,7 @@ public class Player {
     public int getHighestCardValue(ArrayList<Card> cards) {
         int max = 0;
         for (int i = 0; i < cards.size(); i++) {
+            // Check if the rank value is greater than max and if so set it to max rank
             if (Utility.getRankValue(cards.get(i).getRank()) > max) {
                 max = Utility.getRankValue(cards.get(i).getRank());
             }
@@ -171,19 +194,11 @@ public class Player {
         // "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
         // Loop through all the cards
         for (int i = 0; i < allCards.size(); i++) {
-            // System.out.println(allCards.get(i));
             // Get the ranking of the current card as a String and then get the value as a
             // number/index
             int index = Utility.getRankValue(allCards.get(i).getRank());
-            // System.out.println(index);
-            // Ensure that the index in frequencie is not null to prevent errors
-            // if (frequencies[index] != null) {
             // Increment frequency
             frequencies[index] = frequencies[index] + 1;
-            // } else {
-            // // Set to zero if it was previously null
-            // frequencies[index] = 0;
-            // }
         }
         return frequencies;
     }
@@ -197,14 +212,8 @@ public class Player {
             // Get the ranking of the current card as a String and then get the value as a
             // number/index
             int index = Utility.getSuitValue(allCards.get(i).getSuit());
-            // Ensure that the index in frequencie is not null to prevent errors
-            // if (frequencies[index] != null) {
             // Increment frequency
             frequencies[index] = frequencies[index] + 1;
-            // } else {
-            // // Set to zero if it was previously null
-            // frequencies[index] = 0;
-            // }
         }
         return frequencies;
     }
