91b517f6c96c828435b00512de2e90861ace99a1
D2Z11 <133183204+D2Z11@users.noreply.github.com>
2025-03-07 02:17:18 +0000

    Finished

--- src/main/java/com/example/project/Main.java
+++ src/main/java/com/example/project/Main.java
@@ -1,5 +1,6 @@
 package com.example.project;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,40 +8,101 @@ public class Main {
         System.out.println(p);
     }
 
-    public static void main(String[] args) {
+    private static void newGame(Deck deck, Scanner s) {
+        Game game = new Game();
+        Player player = new Player();
+        Player computer = new Player();
+        ArrayList<Card> communityCards = new ArrayList<>();
+        l("Press enter to draw...");
+        s.nextLine();
+        Card d = deck.drawCard();
+        player.addCard(d);
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        for (int i = 0; i < 1; i++) {
+            d = deck.drawCard();
+            player.addCard(d);
+            System.out.println(d.toString());
+            l("Press enter to draw...");
+            s.nextLine();
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
+        for (int i = 0; i < 2; i++) {
+            d = deck.drawCard();
+            computer.addCard(d);
+            System.out.println(d.toString());
+            l("Press enter to draw...");
+            s.nextLine();
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
+        for (int i = 0; i < 3; i++) {
+            d = deck.drawCard();
+            communityCards.add(d);
+            System.out.println(d.toString());
+            l("Press enter to draw...");
+            s.nextLine();
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+        }
+        l("Let's start, shall we?");
         while (true) {
-            l("Would you like to play a game? (y/n)");
-            Scanner s = new Scanner(System.in);
-            if (s.next().equals("y")) {
-                Deck deck = new Deck();
-                deck.initializeDeck();
-                
-                l("I recommend you shuffle first. No funny business, we are playing a game of life or death.");
-                l("Shuffle? (y/n)");
-                if (s.next().equals("y")) {
-                    deck.shuffleDeck();
+            while (true) {
+                l("Would you like to: ");
+                l("View your hand (1)");
+                l("View computer's hand (2)");
+                l("View community cards (3)");
+                l("Exit (4)");
+                l("New game (5)");
+                l("Play your hand (6)");
+                String ans = s.next();
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+                if (ans.equals("1")) {
+                    l(player.getHand().toString());
+                } else if (ans.equals("2")) {
+                    l(computer.getHand().toString());
+                } else if (ans.equals("3")) {
+                    l(communityCards.toString());
+                } else if (ans.equals("4")) {
+                    return;
+                } else if (ans.equals("5")) {
+                    Deck deck2 = new Deck();
+                    deck2.initializeDeck();
+                    deck2.shuffleDeck();
+                    newGame(deck2, s);
+                    return;
+                } else if (ans.equals("6")) {
+                    l(Game.determineWinner(player, computer, player.playHand(communityCards), computer.playHand(communityCards), communityCards));
                 }
-                while (true) {
-                    l("Would you like to: ");
-                    l("View player hand (1)");
-                    l("View community cards (2)");
-                    l("Exit this menu (3)");
-                    if (s.next().equals("3")) {
-                        break;
-                    }
-                }
-
-                System.out.print("Now, keep drawing a card for yourself. I will draw after.");
-                while (true) {
-                    l("\nPress enter to draw...");
-                    s.next();
-                    Card d = deck.drawCard();
-                    System.out.print(d.toString());
-                    
-                }
-            } else {
-                l("Ok... I'll wait.\n\n\n\n\n\n\n\n");
+                l("Press enter to continue");
+                // Twice to avoid problems
+                s.nextLine();
+                s.nextLine();
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
             }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        l("Would you like to play a game? (y/n)");
+        Scanner s = new Scanner(System.in);
+        if (s.next().equals("y")) {
+            Deck deck = new Deck();
+            deck.initializeDeck();
+
+            l("I recommend you shuffle first. No funny business, we are playing a game of life or death.");
+            l("Shuffle? (y/n)");
+            if (s.next().equals("y")) {
+                deck.shuffleDeck();
+            }
+            System.out.print("Now, keep drawing a card for yourself. I will draw after.\n");
+            newGame(deck, s);
+        } else {
+            l("Ok... I'll wait.\n\n\n\n\n\n\n\n");
         }
     }
 }
